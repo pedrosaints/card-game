1513080b6b47a157ddae909227f8693eda5d7437
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2021-08-24 23:06:07 -0300

    Prefab da carta alterada.
    Agora a carta só precisa de uma carta de modelo para ser criada, além de que já vem com o script de DragAndDrop já implementado, bastantando apenas adicionar o evento de drop onde a carta será dropada.

---
+++ CardGame/Assets/DragAndDropScripts/PutCardsInHand.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PutCardsInHand : MonoBehaviour
+{
+
+    private RectTransform cardContainer;
+
+
+    void Start()
+    {
+        cardContainer = GetComponent<RectTransform>();
+    }
+
+
+
+
+}
--- CardGame/Assets/GameModels/CardModel/CardPrefab/CardRenderer.cs
+++ CardGame/Assets/GameModels/CardModel/CardPrefab/CardRenderer.cs
@@ -6,12 +6,14 @@ public class CardRenderer : MonoBehaviour
 {
     public Card cardModel;
 
-    public TMP_Text cardNameTMP;
-    public TMP_Text cardDescriptionTMP;
-    public Image cardArt;
+    private TMP_Text cardNameTMP;
+    private TMP_Text cardDescriptionTMP;
+    private Image cardArt;
 
-    void Start()
+
+    public void Start()
     {
+        GetFields();
         Draw();
     }
 
@@ -19,12 +21,19 @@ public class CardRenderer : MonoBehaviour
     {
         // Insere as informações na carta e desenha ela na tela.
 
-
         // Mudando a fonte e o tamanho da primeira letra do nome da carta.
         cardNameTMP.text = $"<size=120%><font=\"NouveauDropCaps SDF\">{cardModel.cardName[0]}</font></size>{cardModel.cardName.Substring(1)}";
         cardDescriptionTMP.text = cardModel.cardDescription;
         cardArt.sprite = cardModel.cardArt;
         gameObject.SetActive(true);
+    }
+
+    private void GetFields()
+    {
+
+        cardNameTMP = transform.Find("CardNameTMP").GetComponent<TMP_Text>();
+        cardDescriptionTMP = transform.Find("CardDescriptionTMP").GetComponent<TMP_Text>();
+        cardArt = transform.Find("CardArt").GetComponent<Image>();
     }
 
 }
